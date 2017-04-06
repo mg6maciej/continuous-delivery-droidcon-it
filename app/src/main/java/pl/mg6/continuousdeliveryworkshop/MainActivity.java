bf1d2ac9fe83db5dc5533f132d98b3866b2769da
Maciej Górski <maciek.gorski@gmail.com>
2017-04-06 12:24:31 +0200

    Initial project setup.

---
+++ app/src/main/java/pl/mg6/continuousdeliveryworkshop/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.mg6.continuousdeliveryworkshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
