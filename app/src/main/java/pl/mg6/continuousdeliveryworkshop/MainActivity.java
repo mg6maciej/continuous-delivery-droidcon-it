0cd43b34cc552dd8d4544e10ef6c93e84d34e9ce
Maciej Górski <maciek.gorski@gmail.com>
2017-04-06 14:10:12 +0200

    Setup Crashlytics.

--- app/src/main/java/pl/mg6/continuousdeliveryworkshop/MainActivity.java
+++ app/src/main/java/pl/mg6/continuousdeliveryworkshop/MainActivity.java
@@ -3,10 +3,15 @@ package pl.mg6.continuousdeliveryworkshop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Fabric.with(this, new Crashlytics());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
